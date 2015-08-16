a28a5b1eb178a2f4957de21c569b5fab2f63368f
lebull <bulldogboy912000@yahoo.com>
2015-08-16 15:23:30 +0000

    Setting up for deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+export DJANGO_SETTINGS_MODULE=cgbs.settings.prod
+web: gunicorn hellodjango.wsgi --log-file -
--- cgbs/settings/prod.py
+++ cgbs/settings/prod.py
@@ -17,12 +17,24 @@ EMAIL_HOST_PASSWORD = email.EMAIL_HOST_PASSWORD
 EMAIL_USE_TLS = True
 LOGIN_REDIRECT_URL = '/'
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+#https://devcenter.heroku.com/articles/getting-started-with-django#specify-dependencies-with-pip
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

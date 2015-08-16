10e078c24d9e5d4358c249ca49b5f687a97c3315
lebull <bulldogboy912000@yahoo.com>
2015-08-16 15:59:31 +0000

    Adjusting for Deployment

--- cgbs/settings/prod.py
+++ cgbs/settings/prod.py
@@ -10,12 +10,12 @@ TEMPLATE_DEBUG = False
 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = email.EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = email.EMAIL_HOST_PASSWORD
-EMAIL_USE_TLS = True
-LOGIN_REDIRECT_URL = '/'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = email.EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = email.EMAIL_HOST_PASSWORD
+# EMAIL_USE_TLS = True
+# LOGIN_REDIRECT_URL = '/'
 
 #https://devcenter.heroku.com/articles/getting-started-with-django#specify-dependencies-with-pip
 

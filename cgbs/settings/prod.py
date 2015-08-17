9e4ff7f9e396a8238fdbf26a29aeaf99b3fae6c9
lebull <bulldogboy912000@yahoo.com>
2015-08-17 01:49:37 +0000

    Updating for deployment

--- cgbs/settings/prod.py
+++ cgbs/settings/prod.py
@@ -30,12 +30,12 @@ DATABASES['default'] =  dj_database_url.config()
 # # Allow all host headers
 #ALLOWED_HOSTS = ['*']
 
-# # Static asset configuration
-# import os
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = ''
-# STATIC_URL = '/static/'
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

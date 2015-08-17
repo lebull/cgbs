6be895bfd47358793e21c48e28a8b7f9ca18727b
lebull <bulldogboy912000@yahoo.com>
2015-08-17 01:44:33 +0000

    Updating for deployment

--- cgbs/settings/dev.py
+++ cgbs/settings/dev.py
@@ -20,9 +20,8 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
